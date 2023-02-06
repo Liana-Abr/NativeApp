88d562d1314acb0cdd031cda473f868fcce38dd2
Liana <AbramyanLV20@st.ithub.ru>
2023-02-06 17:29:12 +0300

    Login and Reg pages

---
+++ pages/HomeScreen.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import * as React from "react";
+
+export default function HomeScreen({navigation}) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Добро пожаловать на платформу для обучения!</Text>
+            <Button
+                title="Зарегестрироваться"
+                onPress={() =>
+                    navigation.navigate('Auth')
+                }
+            />
+
+            <Text>или</Text>
+            <Button
+            title="Войти"
+            onPress={() =>
+                navigation.navigate('Login')
+            }
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text:{
+        borderColor: '#fdd81a',
+        borderWidth: 2,
+        padding:10 ,
+        fontSize:20,
+        margin: 20,
+        borderRadius: 20,
+
+    },
+    links:{
+        margin: 20,
+        fontSize:10,
+        color:'#fdd81a' ,
+    }
+});
---
+++ pages/LoginScreen.js
@@ -0,0 +1,79 @@
+import { StatusBar } from 'expo-status-bar';
+import {Button, Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+
+export default function LoginScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Вход</Text>
+            <View>
+                <Text style={styles.bold}>Телефон или Логин</Text>
+                <TextInput
+                    keyboardType="default"
+                    style={styles.input}
+                />
+                <Text style={ styles.bold }>Пароль</Text>
+                <TextInput
+                    keyboardType="default"
+                    style={styles.input}
+                />
+                <Pressable style={styles.button} >
+                    <Text style={styles.btnt}>Войти</Text>
+                </Pressable>
+            </View>
+
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text:{
+        margin:30,
+        fontWeight: "bold",
+        fontSize: 30,
+    },
+    bold:{
+
+        fontWeight: "400",
+        margin:5 ,
+        fontSize:20,
+    },
+    input:{
+        width: 250,
+        height:30,
+        marginTop: 20,
+        marginBottom: 20,
+        borderColor: "grey",
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    btn:{
+        padding:20,
+        backgroundColor: "green",
+        borderWidth:2,
+
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: '#f8d428',
+    },
+    btnt: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'black',
+    },
+    other: {
+        marginTop: 15,
+        marginBottom: 5,
+    },
+});
